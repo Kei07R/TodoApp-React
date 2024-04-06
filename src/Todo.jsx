6163412bed2e556d0d07c58b483425679d0cf2b5
EtherK <165754851+Kei07R@users.noreply.github.com>
2024-04-07 02:00:21 +0530

    Added Files of the Todo App

---
+++ src/Todo.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+
+function Todo() {
+
+    const [tasks,setTasks] = useState([]);
+    const [newTask,setNewTask] = useState("");
+
+    const handleInputChange = (event) => {
+        setNewTask(event.target.value);
+    }
+
+    const addTask = () => {
+
+        if(newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
+    }
+
+    const deleteTask = (index) => {
+        const updatedTask = tasks.filter((_,i)=> i !== index);
+        setTasks(updatedTask);
+    }
+
+    const moveTaskUp = (index) => {
+        if (index > 0) {
+            const updatedTask = [...tasks];
+            [updatedTask[index],updatedTask[index-1]] = [updatedTask[index-1],updatedTask[index]]
+            setTasks(updatedTask);
+        }
+    }
+    
+
+    const moveTaskDown = (index) => {
+        if (index < tasks.length - 1) {
+            const updatedTask = [...tasks];
+            [updatedTask[index],updatedTask[index+1]] = [updatedTask[index+1],updatedTask[index]]
+            setTasks(updatedTask);
+        }
+    }
+
+    return(
+    <div className="to-do-list">
+        <h1>To-Do-List</h1>
+
+        <div>
+            <input type="text" placeholder="Enter a task" value={newTask} onChange={handleInputChange} />
+            <button className="add-button" onClick={addTask}>Add Task</button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={()=>deleteTask(index)}>Delete</button>
+                    <button className="move-button" onClick={()=>moveTaskUp(index)}>⬆️</button>
+                    <button className="move-button" onClick={()=>moveTaskDown(index)}>⬇️</button>
+                </li>
+            )}
+        </ol>
+
+    </div>
+    );
+}
+
+export default Todo
